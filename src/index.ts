b318f780960be5babef913974d9d1426a2ab4ccf
ANGEL BELANGEL <125672230+gellybbbb@users.noreply.github.com>
2023-10-25 08:00:30 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import axios from 'axios';
+          /*                                           Client side                                                       */
 
-const userData = {
-  first_name: 'Lisa',
-  last_name: 'Belangel',
-};
+          import axios from 'axios';
 
-// Send a POST request to save user information
-axios.post('http://localhost:3000/save', userData)
-  .then((response: any) => {
-    // Add a type annotation for the 'response' parameter (it may vary based on your API response)
-    console.log(response.data);
-  })
-  .catch((error: any) => {
-    // Add a type annotation for the 'error' parameter (it may vary based on your API error response)
-    console.error(error);
-  });
-
-// Send a GET request to retrieve user information
-axios.get('http://localhost:3000/get')
-  .then((response: any) => {
-    // Add a type annotation for the 'response' parameter (it may vary based on your API response)
-    console.log(response.data);
-  })
-  .catch((error: any) => {
-    // Add a type annotation for the 'error' parameter (it may vary based on your API error response)
-    console.error(error);
-  });
+          const userData = {
+            first_name: 'Ryle',
+            last_name: 'Santos',
+          };
+          
+           // Send a POST request to save user information
+          
+          axios.post('http://localhost:3000/save', userData)
+            .then((response: any) => {
+              console.log(response.data);
+            })
+            .catch((error: any) => {
+              console.error(error);
+            });
+          
+           //Send a GET request to retrieve user information
+          axios.get('http://localhost:3000/get')
+            .then((response: any) => {
+              console.log(response.data);
+            })
+            .catch((error: any) => {
+              console.error(error);
+            });
+          
